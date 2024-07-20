4977849be75fdc4a9c912daaba098608953c166c
Milton Muñoz <livemilton@gmail.com>
2024-07-20 15:44:54 -0500

    add eventListener

--- script.js
+++ script.js
@@ -3,35 +3,29 @@ const h1 = document.querySelector('h1');
 // const parrafito = document.getElementsByClassName('.parrafito');
 // const pid= document.getElementById('pid');
 //const input= document.querySelector('input');
+const form =document.querySelector('#form');
 const input1= document.querySelector('#calculo1');
 const input2= document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult= document.querySelector('#result');
 
+//desde js addEventListener
+form.addEventListener('submit',sumarInputValues);
+// btn.addEventListener('click',btnOnClick1);
+// btn.addEventListener('click',btnOnClick2);
+// btn.addEventListener('click',btnOnClick3);
+
+
+
 //escuchar los eventos 
-function btnOnClick(){
+function sumarInputValues(event){
+    console.log({event})
+    event.preventDefault();
     const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
     pResult.innerText = "Resultado: " +sumaInputs;
  
 }
-//escuchar los eventos 
-function btnOnClick1(){
-    const restaInputs = parseInt(input1.value) - parseInt(input2.value);
-    pResult.innerText = "Resultado: " +restaInputs;
- 
-}
-//escuchar los eventos 
-function btnOnClick2(){
-    const multiplicacionInputs = parseInt(input1.value) * parseInt(input2.value);
-    pResult.innerText = "Resultado: " +multiplicacionInputs;
- 
-}
-//escuchar los eventos 
-function btnOnClick3(){
-    const divisionInputs = parseInt(input1.value) / parseInt(input2.value);
-    pResult.innerText = "Resultado: " +divisionInputs;
- 
-}
+
 
 
 
